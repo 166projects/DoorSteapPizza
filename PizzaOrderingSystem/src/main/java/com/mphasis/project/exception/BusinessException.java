49c99e0cc728ec80ad6f61a8abef3d05f19ec482
riya.bisen <riya.bisen@WKSBAN24TR17016.corp.mphasis.com>
2019-08-08 15:23:38 +0530

    be

--- PizzaOrderingSystem/src/main/java/com/mphasis/project/exception/BusinessException.java
+++ PizzaOrderingSystem/src/main/java/com/mphasis/project/exception/BusinessException.java
@@ -1,5 +1,9 @@
 package com.mphasis.project.exception;
 
-public class BusinessException {
-
+public class BusinessException extends RuntimeException {
+	private static final long serialVersionUID=1L;
+    
+    public BusinessException(String message) {
+ 	   super(message);
+    }
 }
