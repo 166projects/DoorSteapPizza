c74634a5b24ae68111871c33bb9b98c60390ba8c
komal.sharma <komal.sharma@WKSBAN24TR17017.corp.mphasis.com>
2019-08-08 10:43:03 +0530

    updated admin, customer and relationships

--- PizzaOrderingSystem/src/main/java/com/mphasis/project/entities/Admin.java
+++ PizzaOrderingSystem/src/main/java/com/mphasis/project/entities/Admin.java
@@ -13,14 +13,14 @@ public class Admin implements Serializable {
 	private static final long serialVersionUID = 1L;
 		@Id
 		@GeneratedValue(strategy=GenerationType.AUTO)
-		private int id;
+		private String id;
 		private String name;
 		private String password;
 		private long contactno;
-		public int getId() {
+		public String getId() {
 			return id;
 		}
-		public void setId(int id) {
+		public void setId(String id) {
 			this.id = id;
 		}
 		public String getName() {
