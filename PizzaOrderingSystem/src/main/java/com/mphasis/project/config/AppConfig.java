bec24578353cee0665101e4bcc01eda32b003f14
riya.bisen <riya.bisen@WKSBAN24TR17016.corp.mphasis.com>
2019-08-08 15:15:14 +0530

    changes

--- PizzaOrderingSystem/src/main/java/com/mphasis/project/config/AppConfig.java
+++ PizzaOrderingSystem/src/main/java/com/mphasis/project/config/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig {
 	public DriverManagerDataSource getDataSource() {
 		DriverManagerDataSource ds = new DriverManagerDataSource();
 		ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		ds.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		ds.setUrl("jdbc:oracle:thin:@10.96.177.32:1521:xe");
 		ds.setUsername("monty");
 		ds.setPassword("pos");
 		return ds;
@@ -41,7 +41,7 @@ public class AppConfig {
 		props.put("hibernate.hbm2ddl.auto", "create");
 		props.put("hibernate.show_sql", "true");
 		props.put("hibernate.format_sql", "true");
-		props.put("hibernate.default_schema", "monty");
+		
 		sessionFactory.setHibernateProperties(props);
 		sessionFactory.setPackagesToScan("com.mphasis.project.entities");
 		return sessionFactory;
