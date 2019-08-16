677fb553cd0baadc9631e9f5a69eaea767b4dfe1
KomalSharma506 <51989810+KomalSharma506@users.noreply.github.com>
2019-08-16 16:14:58 +0530

    Delete CCDetailsBo.java

--- PizzaOrderingSystem/src/main/java/com/mphasis/project/bo/CCDetailsBo.java
+++
@@ -1,9 +0,0 @@
-package com.mphasis.project.bo;
-
-import com.mphasis.project.entities.CreditCardDetails;
-
-public interface CCDetailsBo {
-	 public void insertCreditCardDetails(CreditCardDetails ccd) throws Exception;
-     public void removeCreditCardDetails(long card_no);
-     
-}
